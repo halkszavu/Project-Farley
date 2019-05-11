a1a03bae86473997e3814fad5206ba94e2b98d30
Botond Lajos Perényi <halkszavu@outlook.com>
2019-05-11 13:44:48 +0200

    Basic DbContext (Wizard)

--- WebApi.DAL/Velho.cs
+++ WebApi.DAL/Velho.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WebApi.DAL
 {
-    public class Velho
+    public class VelhoContext : DbContext
     {
+        const string connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        private ILoggerFactory GetLoggerFactory()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder => builder.AddConsole()
+                                                           .AddFilter(ll => ll == LogLevel.Debug));
+            return serviceCollection.BuildServiceProvider()
+                .GetService<ILoggerFactory>();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
