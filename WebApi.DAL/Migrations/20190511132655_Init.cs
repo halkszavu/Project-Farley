38afa1141f6bb3ecf564bc9c08c4fde06cd59696
Botond Lajos Perényi <halkszavu@outlook.com>
2019-05-11 15:29:16 +0200

    Web API init, Code-First migration added and applied.

---
+++ WebApi.DAL/Migrations/20190511132655_Init.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.DAL.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meetings",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Place = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meetings", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Populii",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    MartialState = table.Column<string>(nullable: false),
+                    SiblingState = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Populii", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Notes = table.Column<string>(nullable: true),
+                    Time = table.Column<DateTime>(nullable: false),
+                    PersonID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Notes_Populii_PersonID",
+                        column: x => x.PersonID,
+                        principalTable: "Populii",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonMeeting",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PersonID = table.Column<int>(nullable: false),
+                    MeetingID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonMeeting", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PersonMeeting_Meetings_MeetingID",
+                        column: x => x.MeetingID,
+                        principalTable: "Meetings",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonMeeting_Populii_PersonID",
+                        column: x => x.PersonID,
+                        principalTable: "Populii",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_PersonID",
+                table: "Notes",
+                column: "PersonID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonMeeting_MeetingID",
+                table: "PersonMeeting",
+                column: "MeetingID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonMeeting_PersonID",
+                table: "PersonMeeting",
+                column: "PersonID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "PersonMeeting");
+
+            migrationBuilder.DropTable(
+                name: "Meetings");
+
+            migrationBuilder.DropTable(
+                name: "Populii");
+        }
+    }
+}
