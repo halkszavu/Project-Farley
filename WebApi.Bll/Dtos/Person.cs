c9d3b950b6493a9885b2710164d35d7796f94024
Botond Lajos Perényi <halkszavu@outlook.com>
2019-05-14 10:59:10 +0200

    Data Annotation, only for Person

--- WebApi.Bll/Dtos/Person.cs
+++ WebApi.Bll/Dtos/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using WebApi.Entities;
 
 namespace WebApi.Bll.Dtos
@@ -7,6 +8,7 @@ namespace WebApi.Bll.Dtos
     public class Person
     {
         public int ID { get; set; }
+        [Required(ErrorMessage ="Person name is mandatory", AllowEmptyStrings = false)]
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
         public MartialState MartialState { get; set; }
