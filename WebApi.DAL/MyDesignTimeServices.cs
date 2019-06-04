d8c727137a7ffb581f5633e9d871615d3ab6b6ff
Botond Lajos Perényi <halkszavu@outlook.com>
2019-06-04 23:36:10 +0200

    Pluralization added

---
+++ WebApi.DAL/MyDesignTimeServices.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApi.DAL
+{
+    public class MyDesignTimeServices : IDesignTimeServices
+    {
+        public void ConfigureDesignTimeServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IPluralizer, MyPluralizer>();
+        }
+    }
+}
---
+++ WebApi.DAL/MyPluralizer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApi.DAL
+{
+    public class MyPluralizer : IPluralizer
+    {
+        public string Pluralize(string name)
+        {
+            return Inflector.Pluralize(name) ?? name;
+        }
+
+        public string Singularize(string name)
+        {
+            return Inflector.Singularize(name) ?? name;
+        }
+    }
+}
